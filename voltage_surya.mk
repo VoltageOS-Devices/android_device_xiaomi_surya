52dc7fe59cf2c83c2181ce1e1eb6f90af99856c6
Cow6oy <mezackisilva@gmail.com>
2022-08-15 19:13:36 -0300

    surya: lineage -> voltage Signed-off-by: Mezaque_Silver <mezackisilva@gmail.com>

--- voltage_surya.mk
+++ voltage_surya.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_surya
+PRODUCT_NAME := voltage_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
