bc99ae451dce7f553d144e22efc3de89dd844a23
vyyz <mezackisilva@gmail.com>
2022-09-10 12:53:21 -0300

    Revert "surya: Disable kpti"
    This reverts commit f0d7ca6b58ad53e848ca7742ff9732aa2d375190.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,7 +118,6 @@ BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
-BOARD_KERNEL_CMDLINE += kpti=off
 
 # LMKD
 TARGET_LMKD_STATS_LOG := true
